907b42c8e7569cf5666beebc34caf78527c8ab5d
Salwa Hegazy <eng.salwahegazy92@gmail.com>
2022-11-13 20:03:03 +0200

    finish ConvertCurrency UI

--- CurrencyApp/Convert Currency/View/Controller + Storyboard/ConvertCurrencyView.swift
+++ CurrencyApp/Convert Currency/View/Controller + Storyboard/ConvertCurrencyView.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import DropDown
 
 class ConvertCurrencyView: UIViewController {
-
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var fromDropDownView: UIView!
+    @IBOutlet weak var toDropDownView: UIView!
+    @IBOutlet weak var fromSelectedLabel: UILabel!
+    @IBOutlet weak var toSelectedLabel: UILabel!
+    
+    //MARK: - Members
+    
+    let fromDropDown = DropDown()
+    let toDropDown   = DropDown()
+    let data : [String] = []
+    
+    //MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUpDropDownViews()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func fromDropdownBtnPressed(_ sender: Any) {
+        fromDropDown.show()
+    }
+    
+    @IBAction func toDropdownBtnPressed(_ sender: Any) {
+        toDropDown.show()
+    }
+    
+    @IBAction func detailsBtnPressed(_ sender: Any) {
         
     }
     
+    //MARK: - Methods
+    
+    func setUpDropDownViews() {
+        fromDropDown.anchorView = fromDropDownView
+        toDropDown.anchorView = toDropDownView
+        fromDropDown.dataSource = data
+        toDropDown.dataSource = data
+        fromDropDown.direction = .bottom
+        toDropDown.direction = .bottom
+        fromDropDown.bottomOffset = CGPoint(x: 0, y:(fromDropDown.anchorView?.plainView.bounds.height)!)
+        toDropDown.bottomOffset = CGPoint(x: 0, y:(toDropDown.anchorView?.plainView.bounds.height)!)
+        // get selected value from dropdown
+        fromDropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+            fromSelectedLabel.text = item
+        }
+
+        toDropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+            toSelectedLabel.text = item
+
+        }
+
+    }
 
 }
